56bf2d2762af9eb2c77f17c0d459133118f9ffe1
Hernan Di Tano <hditano@gmail.com>
2024-06-21 22:16:19 +0200

    added plot and database calls via menu

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import psutil
 from psutil._common import bytes2human
 import math
 import sqlite3
-import os
+import plotext as plt
 
 class SystemInfo:
     def __init__(self) -> None:
@@ -26,6 +26,40 @@ class SystemInfo:
         print(f'Memory: {self.memory.percent}%')
     
         print(f'Disk Free Space: {bytes2human(self.disk.free)}')
+        
+        self.Menu()
+
+    def Menu(self):
+        print("========Menu==========")
+        print("(1) Check Data in Db")
+        print("(2) Check Stats in a Diagram")
+        print("(3) Draw Plot")
+        choose = input("Please choose an option")
+        print(type(choose))
+
+        match choose:
+            case '1':
+                print("You choose Add Data")
+            case '2':
+                self.GetStats()
+            case '3':
+                self.DrawPlot()
+            case _:
+                print('none')
+                
+
+    def DrawPlot(self):
+        plt.xlabel('Time Past')
+        plt.ylabel('Hz')
+        
+        data_hz = [1400, 1800, 3200, 900]
+        
+        plt.plot(data_hz)
+
+        plt.canvas_color('black')
+        plt.axes_color('gray+')
+        plt.title("Sclatter Plot")
+        plt.show()
 
     def InitDb(self):
         self.cur.execute("""
@@ -45,6 +79,10 @@ class SystemInfo:
             (self.cpu_count, self.cpu_freq.current, self.cpu_load[0], self.memory.percent, self.disk.free)
         )
         self.con.commit()
+
+    def GetStats(self):
+        x = self.cur.execute("SELECT frequency FROM stats")
+        print((x.fetchall()))
 
 
 SysInstance = SystemInfo()
